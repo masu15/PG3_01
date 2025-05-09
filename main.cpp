bb36b5a22c3c6bb15dac121080a130afad194c47
ます <k024g1021@g.neec.ac.jp>
2025-05-09 15:20:22 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include"stdio.h"
 int Recursive1(int kyyuryou, int zikan, int kotei)
 {
+	
 	if (kyyuryou >= kotei)
 	{
 		printf("%dŠÔŒã‚É’´‚¦‚é", zikan);
 		return zikan;
 	}
+	
 	printf("%d\n%d\n", kyyuryou, kotei);
-	kyyuryou += kyyuryou * 2 - 50;
+	kyyuryou= kyyuryou * 2 - 50;
 	return(Recursive1(kyyuryou, ++zikan, kotei +1072));
 }
 int main()
 {
 	int saiki = 100;
 	int ippan = 1072;
-	int zikan = 1;
+	int zikan = 0;
 	int result1 = Recursive1(saiki, zikan, ippan);
+
+	return 0;
 }
